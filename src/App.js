61c46fcb6550e2d064bc31c56e0013d69f17205b
kamlesh <kamlesh.kumawat@axa-abs-co.in>
2023-01-06 00:50:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
+import React, { useState, useCallback } from 'react'
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
+
+  const [dataList, setResult] = useState([])
+
+  function getJokes(searchTerm, limit = 15) {
+    fetch(`https://icanhazdadjoke.com/search?term=${searchTerm}&limit=${limit}`,{
+        method: 'GET',
+        headers: {
+          Accept: 'application/json'
+        }
+    }).then(response => response.json())
+      .then(json => {
+        const jokes = json.results;
+        setResult(jokes)
+      });
+  };
+
+  const debounce = (func, dealy) => {
+    let timer;
+    return function (...args) {
+      const context = this;
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        func.apply(context, args)
+      }, dealy);
+    };
+  };
+  // eslint-disable-next-line
+  const debounceFunction = useCallback(debounce(getJokes, 200));
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Debounceing <code>React.js</code> and Hook.
         </p>
-        <a
+        <input
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          type="text"
+          name="search"
+          onChange={(e) => debounceFunction(e.target.value)}
+        />
+         <ul>
+            {dataList && dataList.map((item, index) => (
+              <li key={index}>{item.joke}</li>
+            ))}
+         </ul>
       </header>
     </div>
   );
